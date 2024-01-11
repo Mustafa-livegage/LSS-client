1312ca0b1f6e6323f0d6a5f078cbf85f8cbae107
Mustafa-livegage <mpanjiwala@livegage.ai>
2024-01-11 16:22:07 +0530

    Addedrouter and search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./screens/HomePage/HomePage";
+import SingleEntryForm from "./screens/SingleEntryForm/SingleEntryForm";
+
 import "./styles.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <HomePage />
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/addloans" element={<SingleEntryForm />} />
+        </Routes>
+      </Router>
     </>
   );
 }
