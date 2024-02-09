94c8b8445e46d58c2d549dec3cd4539af7b68000
samarpit sharma <SASharma@livegage.ai>
2024-02-09 15:36:18 +0530

    fixed unauthorized page

--- src/screens/Unauthorized.jsx
+++ src/screens/Unauthorized.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { ImBlocked } from "react-icons/im";
 
 const Unauthorized = () => {
-  return <div>Unauthorized</div>;
+  return (
+    <Container className="vh-100  d-flex flex-column  align-items-center justify-content-center ">
+      <div>
+        <ImBlocked size={150} />
+      </div>
+      <br />
+      <div>
+        <h1 className="fs-1 fw-bolder ">Unauthorized</h1>
+      </div>
+    </Container>
+  );
 };
 
 export default Unauthorized;
